7cf2d35dc9545de24ebe41ae8ba2cf222211a0eb
mikhail-create <leonovm143@gmail.com>
2022-03-06 15:53:21 +0600

    added icons

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,11 +1,12 @@
 import { ErrorMessage, Field, Form, Formik } from 'formik'
 import React, { useState } from 'react'
+import { MdMail, MdPerson, MdVpnKey } from "react-icons/md"
 import { authService } from '../../_services/auth.service'
 import styles from './loginpage.module.scss'
 
 function LoginPage() {
 
-const [status, setStatus] = useState("")
+    const [status, setStatus] = useState("")
 
     return (
         <div className={styles.login}>
@@ -16,7 +17,7 @@ const [status, setStatus] = useState("")
                         .then(
                             user => {
                                 console.log(user);
-                                
+
                             },
                             error => {
                                 setStatus(error)
@@ -30,10 +31,18 @@ const [status, setStatus] = useState("")
                     </span>
 
                     <div className={styles.input_container}>
-                        <Field className={`${styles.input__control} ${status ? styles.error : ''}`} name="email" type="email" placeholder="Email" />
-                        <Field className={`${styles.input__control} ${status ? styles.error : ''}`} name="password" type="password" placeholder="Password" />
+                        <div className={styles.input_container__field}>
+                            <MdMail className={styles.input_container__icon} size='20px' />
+                            <Field className={`${styles.input__control} ${status ? styles.error : ''}`} name="email" type="email" placeholder="Email" />
+                        </div>
+                        <div className={styles.input_container__field}>
+                            <MdVpnKey className={styles.input_container__icon} size='20px' />
+                            <Field className={`${styles.input__control} ${status ? styles.error : ''}`} name="password" type="password" placeholder="Password" />
+                        </div>
                     </div>
+
                     <button className={styles.form__button} type="submit">Submit</button>
+
                     <div className={styles.login_links}>
                         <span className={styles.login_links__item}>
                             <a href='#'>
@@ -42,7 +51,7 @@ const [status, setStatus] = useState("")
                         </span>
                         <span className={styles.login_links__item}>
                             <a href='/registration'>
-                                Don't have account?
+                                Don't have an account?
                             </a>
                         </span>
                         {status ? <div className={styles.alert}>{status}</div> : <br></br>}
--- src/pages/registation/RegistrationPage.tsx
+++ src/pages/registation/RegistrationPage.tsx
@@ -1,12 +1,13 @@
 import { ErrorMessage, Field, Form, Formik } from 'formik'
 import React, { useState } from 'react'
+import { MdMail, MdPerson } from 'react-icons/md'
 import { authService } from '../../_services/auth.service'
 import styles from './registrationpage.module.scss'
 
 function RegistrationPage() {
 
     const [status, setStatus] = useState("")
-    
+
     return (
         <div className={styles.registration}>
             <Formik
@@ -16,7 +17,7 @@ function RegistrationPage() {
                         .then(
                             user => {
                                 console.log(user);
-                                
+
                             },
                             error => {
                                 setStatus(error)
@@ -28,17 +29,28 @@ function RegistrationPage() {
                     <span className={styles.form_header}>
                         <h1>Registration</h1>
                     </span>
-                    
+
                     <div className={styles.input_container}>
-                        <Field className={`${styles.input__control} ${status ? styles.error : ''}`} name="name" type="text" placeholder="Name" />
-                        <Field className={`${styles.input__control} ${status ? styles.error : ''}`} name="email" type="email" placeholder="Email" />
-                        <Field className={`${styles.input__control} ${status ? styles.error : ''}`} name="password" type="password" placeholder="Password" />
+                        <div className={styles.input_container__field}>
+                            <MdPerson className={styles.input_container__icon} size='20px' />
+                            <Field className={`${styles.input__control} ${status ? styles.error : ''}`} name="name" type="text" placeholder="Name" />
+                        </div>
+                        <div className={styles.input_container__field}>
+                            <MdMail className={styles.input_container__icon} size='20px' />
+                            <Field className={`${styles.input__control} ${status ? styles.error : ''}`} name="email" type="email" placeholder="Email" />
+                        </div>
+                        <div className={styles.input_container__field}>
+                            <MdPerson className={styles.input_container__icon} size='20px' />
+                            <Field className={`${styles.input__control} ${status ? styles.error : ''}`} name="password" type="password" placeholder="Password" />
+                        </div>
                     </div>
+
                     <button className={styles.form__button} type="submit">Submit</button>
+
                     <div className={styles.login_links}>
                         <span className={styles.login_links__item}>
                             <a href='/login'>
-                                Already have account?
+                                Already have an account?
                             </a>
                         </span>
                         {status ? <div className={styles.alert}>{status}</div> : <br></br>}
